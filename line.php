eb81f8f1278656545caf3a3150a1a3391df416f7
vanichasingtothong <68370175+vanichasingtothong@users.noreply.github.com>
2020-07-16 15:27:13 +0700

    Update line.php

--- line.php
+++ line.php
@@ -59,7 +59,13 @@ if ( sizeof($request_array['events']) > 0 )
 	   if($text == "ใบอนุญาตขายสุราของฉันหมดอายุวันไหน"){
 		   $reply_message = 'ฉันขอเลขที่ใบอนุญาตของคุณเพื่อทำการตรวจสอบ';
 	   }	   
-	    
+	   
+	   if($text == "อุณหภุมิตอนนี้"){
+		   $reply_message = '42 องศาเซลเซียส';
+	   }	   
+	   
+	   
+	   
 	   $split_text = explode(" ", $text);
 	   //foreach ($split_text as $t){$reply_message = $t;}
 	   if (in_array("ฉันหาเลขใบอนุญาตของฉันไม่เจอ", $split_text)) { 
